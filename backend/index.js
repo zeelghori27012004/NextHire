e6ecac547a3b7848ba6116cce6d99faf2aeedc8c
zeelghori27012004 <ghorizeeljkvrz@gmail.com>
2025-05-26 13:49:45 +0530

    started backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+const PORT = 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running at port ${PORT}`);
+})
