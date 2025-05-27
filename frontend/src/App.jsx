52f93f2fba8bff97ad21f3ed70f35fea972475b9
zeelghori27012004 <ghorizeeljkvrz@gmail.com>
2025-05-27 16:16:16 +0530

    starting frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-red-500'>Lets build job portal </h1>
+    </>
+  )
+}
+
+export default App
