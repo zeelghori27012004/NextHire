feaad4f41d5e43e3c951ee7e38636aa66e04b490
zeelghori27012004 <ghorizeeljkvrz@gmail.com>
2025-05-28 21:41:13 +0530

    added component for job deccription

--- backend/index.js
+++ backend/index.js
@@ -27,10 +27,10 @@ const PORT = process.env.PORT || 3000;
 
 
 // api's
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/job", jobRoute);
-app.use("/api/v1/application", applicationRoute);
+app.use("/api/user", userRoute);
+app.use("/api/company", companyRoute);
+app.use("/api/job", jobRoute);
+app.use("/api/application", applicationRoute);
 
 
 
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,1 +1,4 @@
 export const USER_API_END_POINT="http://localhost:8000/api/user";
+export const JOB_API_END_POINT="http://localhost:8000/api/job";
+export const APPLICATION_API_END_POINT="http://localhost:8000/api/application";
+export const COMPANY_API_END_POINT="http://localhost:8000/api/company";
