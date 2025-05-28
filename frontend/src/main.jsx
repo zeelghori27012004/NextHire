744aad7519be0e453e4e356ae0e301fd490a509b
zeelghori27012004 <ghorizeeljkvrz@gmail.com>
2025-05-28 09:11:05 +0530

    added footer component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from './components/ui/sonner'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <App />
     <Toaster />
+    </Provider>
   </StrictMode>,
 )
