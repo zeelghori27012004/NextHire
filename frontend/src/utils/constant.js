ed22800322d69405a338e2a3f4dff9a56b8068d9
zeelghori27012004 <ghorizeeljkvrz@gmail.com>
2025-05-29 22:07:56 +0530

    vercel deplouyment routes

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://nexthire-backend-y9o0.onrender.com/api/user";
-export const JOB_API_END_POINT="https://nexthire-backend-y9o0.onrender.com/api/job";
-export const APPLICATION_API_END_POINT="https://nexthire-backend-y9o0.onrender.com/api/application";
-export const COMPANY_API_END_POINT="https://nexthire-backend-y9o0.onrender.com/api/company";
+export const USER_API_END_POINT="next-hire-backend-psbrxs3w8-zeel-ghoris-projects.vercel.app/api/user";
+export const JOB_API_END_POINT="next-hire-backend-psbrxs3w8-zeel-ghoris-projects.vercel.app/api/job";
+export const APPLICATION_API_END_POINT="next-hire-backend-psbrxs3w8-zeel-ghoris-projects.vercel.app/api/application";
+export const COMPANY_API_END_POINT="next-hire-backend-psbrxs3w8-zeel-ghoris-projects.vercel.app/api/company";
