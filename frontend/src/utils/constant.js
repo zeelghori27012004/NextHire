fb7e1f269c77b3e3e14808016ca97e1d882421a2
zeelghori27012004 <ghorizeeljkvrz@gmail.com>
2025-05-29 23:04:55 +0530

    added env files on frontend

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,6 @@
-export const USER_API_END_POINT="next-hire-backend-psbrxs3w8-zeel-ghoris-projects.vercel.app/api/user";
-export const JOB_API_END_POINT="next-hire-backend-psbrxs3w8-zeel-ghoris-projects.vercel.app/api/job";
-export const APPLICATION_API_END_POINT="next-hire-backend-psbrxs3w8-zeel-ghoris-projects.vercel.app/api/application";
-export const COMPANY_API_END_POINT="next-hire-backend-psbrxs3w8-zeel-ghoris-projects.vercel.app/api/company";
+const BASE_API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export const USER_API_END_POINT = `${BASE_API_URL}/api/user`;
+export const JOB_API_END_POINT = `${BASE_API_URL}/api/job`;
+export const APPLICATION_API_END_POINT = `${BASE_API_URL}/api/application`;
+export const COMPANY_API_END_POINT = `${BASE_API_URL}/api/company`;
